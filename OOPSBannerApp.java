56dea4587b23f944d46f95e1b057dc9cb19738ae
as9831-maker <as9831@srmist.edu.in>
2026-03-03 12:18:37 +0530

    UC3: Render OOPS banner using String.join()

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This program prints the text "OOPS" to the console.
+ * 
+ * @author Adhiti Sorab
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
